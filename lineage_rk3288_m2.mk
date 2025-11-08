82a677eed4e70cb40b8d083c44a0d885ada6e616
AceoS <42939027+AceoS@users.noreply.github.com>
2025-11-08 20:06:12 +0530

    Refactor lineage_rk3288_m2.mk for product details
    Updated product model and manufacturer names for consistency. Inherited common Lineage configurations.

--- lineage_rk3288_m2.mk
+++ lineage_rk3288_m2.mk
@@ -1,10 +1,18 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+# Copyright (C) 2025 AceoS
+# SPDX-License-Identifier: Apache-2.0
 
+# Inherit Lineage common + device
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, device/rockchip/rk3288_m2/device.mk)
+
+# Device overrides
 PRODUCT_NAME := lineage_rk3288_m2
 PRODUCT_DEVICE := rk3288_m2
 PRODUCT_BRAND := rockchip
-PRODUCT_MODEL := RK3288 M2
-PRODUCT_MANUFACTURER := rockchip
+PRODUCT_MODEL := RK3288-M2
+PRODUCT_MANUFACTURER := Rockchip
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilt/dt.img:dt.img
+# Build props (customize build fingerprint)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="rk3288_m2-user 9 PPR1.180610.011 eng.aceos.20251108.143000 test-keys" \
+    BUILD_FINGERPRINT="rockchip/rk3288_m2/rk3288_m2:9/PPR1.180610.011/aceos20251108:eng/test-keys"
