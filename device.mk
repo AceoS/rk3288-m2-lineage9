38e7ef93d5da7b86c565d5603240dc0b71ba23cf
AceoS <42939027+AceoS@users.noreply.github.com>
2025-11-08 20:05:58 +0530

    Update device.mk with new product configurations

--- device.mk
+++ device.mk
@@ -1,14 +1,53 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+# Copyright (C) 2025 AceoS
+# SPDX-License-Identifier: Apache-2.0
 
-PRODUCT_COPY_FILES += \
-    device/rockchip/rk3288_m2/prebuilt/firmware/brcmfmac43362-sdio.bin:system/etc/firmware/brcmfmac43362-sdio.bin \
-    device/rockchip/rk3288_m2/prebuilt/firmware/brcmfmac43362-sdio.txt:system/etc/firmware/brcmfmac43362-sdio.txt \
-    device/rockchip/rk3288_m2/prebuilt/lib/libMali.so:system/lib/libMali.so \
-    device/rockchip/rk3288_m2/prebuilt/lib/egl/libGLES_mali.so:system/lib/egl/libGLES_mali.so
+# Inherit AOSP base + BoardConfig
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
+$(call inherit-product, device/rockchip/rk3288_m2/BoardConfig.mk)
 
+# Device identifier (shared with lineage.mk)
+PRODUCT_DEVICE := rk3288_m2
+PRODUCT_BRAND := rockchip
+PRODUCT_MANUFACTURER := Rockchip
+
+# Overlays (empty dir for now; add if needed for themes)
+DEVICE_PACKAGE_OVERLAYS += device/rockchip/rk3288_m2/overlay
+
+# Screen
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xxhdpi
+
+# Basic packages
 PRODUCT_PACKAGES += \
-    brcmfmac.ko
+    Launcher3 \
+    Settings \
+    SystemUI \
+    ExactCalculator \
+    LatinIME
 
+# RIL (for telephony; remove if no modem)
+PRODUCT_PACKAGES += \
+    android.hardware.radio@1.0 \
+    android.hardware.radio.config@1.0
+
+# Audio (Rockchip defaults)
+PRODUCT_PACKAGES += \
+    audio.primary.rk3288 \
+    audio.usb.default \
+    audio_policy_configuration.xml
+
+# Camera (basic HAL)
+PRODUCT_PACKAGES += \
+    camera.rk3288 \
+    libcamera_client
+
+# GPS/WiFi (add if supported)
+PRODUCT_PACKAGES += \
+    gps.rk3288 \
+    libwpa_client \
+    wpa_supplicant \
+    wpa_supplicant.conf
+
+# DTB copy to boot.img
 PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
-    frameworks/native/data/etc/android.hardware.ethernet.xml:system/etc/permissions/android.hardware.ethernet.xml
+    $(LOCAL_PATH)/prebuilt/dt.img:dt.img
