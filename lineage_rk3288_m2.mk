0dbede6eb51e558cd4f6147dc6b43fc298faf4da
AceoS <42939027+AceoS@users.noreply.github.com>
2025-11-08 14:03:35 +0530

    Create lineage_rk3288_m2.mk

---
+++ lineage_rk3288_m2.mk
@@ -0,0 +1,19 @@
+# lineage_rk3288_m2.mk
+
+$(call inherit-product, device/rockchip/rk3288_m2/device.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+
+PRODUCT_NAME := lineage_rk3288_m2
+PRODUCT_DEVICE := rk3288_m2
+PRODUCT_BRAND := rockchip
+PRODUCT_MODEL := RK3288 M2 Custom
+PRODUCT_MANUFACTURER := Rockchip
+
+# Build Fingerprint (from your 4.4)
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="rk3288-user 4.4.4 KTU84P eng.root.20181226.170730 release-keys"
+
+BUILD_FINGERPRINT := rockchip/rk3288/rk3288:4.4.4/KTU84P/eng.root.20181226.170730:user/release-keys
+
+# GApps (Optional)
+$(call inherit-product-if-exists, vendor/gapps/arm/gapps-arm-9.0.mk)
