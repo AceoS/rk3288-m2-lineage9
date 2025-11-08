e689a421f695dc05929cb9b36bde16f3c72f8f91
AceoS <42939027+AceoS@users.noreply.github.com>
2025-11-08 16:54:13 +0530

    Update lineage_rk3288_m2.mk

--- lineage_rk3288_m2.mk
+++ lineage_rk3288_m2.mk
@@ -1,13 +1,10 @@
-$(call inherit-product, device/rockchip/rk3288_m2/device.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 PRODUCT_NAME := lineage_rk3288_m2
 PRODUCT_DEVICE := rk3288_m2
 PRODUCT_BRAND := rockchip
-PRODUCT_MODEL := RK3288 M2 Custom
-PRODUCT_MANUFACTURER := Rockchip
+PRODUCT_MODEL := RK3288 M2
+PRODUCT_MANUFACTURER := rockchip
 
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="rk3288-user 4.4.4 KTU84P eng.root.20181226.170730 release-keys"
-
-BUILD_FINGERPRINT := rockchip/rk3288/rk3288:4.4.4/KTU84P/eng.root.20181226.170730:user/release-keys
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/prebuilt/dt.img:dt.img
