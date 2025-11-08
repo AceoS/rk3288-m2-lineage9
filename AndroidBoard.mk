2629a69ca9d61f35241f0bdd798ae746a043b470
AceoS <42939027+AceoS@users.noreply.github.com>
2025-11-08 20:05:05 +0530

    Add AndroidProducts.mk file

--- AndroidBoard.mk
+++
@@ -1,3 +0,0 @@
-# AndroidProducts.mk
-PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_rk3288_m2.mk
---
+++ AndroidProducts.mk
@@ -0,0 +1,5 @@
+#
+# This file lists all product makefiles for the rk3288_m2 device.
+#
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_rk3288_m2.mk
